bc8d36172122be0bf13af333c1866e90fd4314c2
edwinchen85 <edwinchen85@gmail.com>
2017-05-31 01:00:39 +0800

    Define $scope.search func

--- script.js
+++ script.js
@@ -27,4 +27,26 @@ app.controller('MainCtrl', function($scope, $http, $timeout) {
       return;
     }
   });
+
+  $scope.search = function() {
+    $scope.result = [];
+    help.addClass('hide');
+    search.removeClass('fullHeight');
+    var title = input.val();
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=JSON_CALLBACK';
+    var page = 'https://en.wikipedia.org/?curid=';
+
+    $http.jsonp(api + title + cb)
+    .success(function(data) {
+      var results = data.query.pages;
+      angular.forEach(results, function(v,k) {
+        $scope.results.push({
+          title: v.title,
+          body: v.extract,
+          page: page + v.pageid
+        });
+      });
+    });
+  }
 });
